f387ffe5c3a355cd8d1e42fc5284d5278245f45c
Dev Shrut Jain <138303636+jaindevshrut@users.noreply.github.com>
2025-03-21 00:01:54 +0530

    added user account management routes for password change, user details update, avatar and cover image uploads, and retrieving current user and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from "express"
-import { registerUser, loginUser, logoutUser,refreshAccessToken } from "../controllers/user.controller.js"
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router()
@@ -23,4 +35,13 @@ router.route("/login").post(
 // Secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/udate-account").patch(verifyJWT,updateUserDetails) // !isko patch isliye kiya h kyuki kuch fields update krni h nhi toh post me sb update ho jayeg
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile) // isme username ham param se le rhe h toh //! : likha imp h
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
