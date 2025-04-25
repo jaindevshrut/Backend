49db7c298ce25b30ef62b3e8aa0346da5e537172
Dev Shrut Jain <138303636+jaindevshrut@users.noreply.github.com>
2025-04-25 16:42:55 +0530

    Update index.js for render

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config({path: "../env"})
 import connectDB from "./db/dbConnect.js";
 import app from "./app.js"
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 connectDB().then(()=>{
     app.on("error", (error)=>{
         console.error("Error: ", error)
